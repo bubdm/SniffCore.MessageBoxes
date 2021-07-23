da03f53529a3d778e2c6d0edcd2980d087eb1758
David Wendland <the.devicenator@gmail.com>
2021-07-23 20:23:38 +0200

    Add a IWPFMessageBoxWrapper to unit test the WPFMessageBox.

---
+++ SniffCore.MessageBoxes/IWPFMessageBoxWrapper.cs
@@ -0,0 +1,258 @@
+﻿//
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Windows;
+
+namespace SniffCore.MessageBoxes
+{
+    /// <summary>
+    ///     Show a WPF window as a message box which is full configurable.
+    /// </summary>
+    /// <example>
+    ///     <code lang="csharp">
+    /// <![CDATA[
+    /// public class ViewModel : ObservableObject
+    /// {
+    ///     private IWPFMessageBoxWrapper _messageBox
+    ///     public ViewModel(IWPFMessageBoxWrapper messageBox)
+    ///     {
+    ///         _messageBox = messageBox;
+    ///     }
+    /// 
+    ///     public void Show()
+    ///     {
+    ///         messageBox.Show("Messagebox Text");
+    ///     }
+    /// }
+    ///
+    /// [TestFixture]
+    /// public class ViewModelTests
+    /// {
+    ///     private Mock<IWPFMessageBoxWrapper> _messageBox;
+    ///     private ViewModel _target;
+    ///
+    ///     [SetUp]
+    ///     public void Setup()
+    ///     {
+    ///         _messageBox = new Mock<IWPFMessageBoxWrapper>();
+    ///         _target = new ViewModel(_messageBox.Object);
+    ///     }
+    ///
+    ///     [Test]
+    ///     public void Show_Called_ShowsMessageBox()
+    ///     {
+    ///         _target.Show();
+    ///
+    ///         _messageBox.Verify(x => x.Show("Messagebox Text"), Times.Once);
+    ///     }
+    /// }
+    /// ]]>
+    /// </code>
+    /// </example>
+    public interface IWPFMessageBoxWrapper
+    {
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <param name="defaultButton">A WPFMessageBoxResult value that specifies the default result of the message box.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <param name="defaultButton">A WPFMessageBoxResult value that specifies the default result of the message box.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <param name="defaultButton">A WPFMessageBoxResult value that specifies the default result of the message box.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxOptions options);
+
+        /// <summary>
+        ///     Displays a message box that has a message and that returns a result of the button the user pressed.
+        /// </summary>
+        /// <param name="owner">A Window that represents the owner window of the message box.</param>
+        /// <param name="messageBoxText">A String that specifies the text to display.</param>
+        /// <param name="caption">A String that specifies the title bar caption to display.</param>
+        /// <param name="buttons">A WPFMessageBoxButtons value that specifies which buttons to display.</param>
+        /// <param name="icon">A WPFMessageBoxImages value that specifies the icon to display.</param>
+        /// <param name="defaultButton">A WPFMessageBoxResult value that specifies the default result of the message box.</param>
+        /// <param name="options">A WPFMessageBoxOptions value object that specifies the options.</param>
+        /// <returns>A WPFMessageBoxResult value that specifies which message box button is clicked by the user.</returns>
+        /// <exception cref="ArgumentNullException">options is null.</exception>
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton, WPFMessageBoxOptions options);
+    }
+}
---
+++ SniffCore.MessageBoxes/WPFMessageBoxWrapper.cs
@@ -0,0 +1,133 @@
+﻿//
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+using System.Windows;
+
+namespace SniffCore.MessageBoxes
+{
+    /// <inheritdoc />
+    public class WPFMessageBoxWrapper : IWPFMessageBoxWrapper
+    {
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText)
+        {
+            return WPFMessageBox.Show(messageBoxText);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption, buttons);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption, buttons, icon);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption, buttons, icon, defaultButton);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption, buttons);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption, buttons, icon);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption, buttons, icon, defaultButton);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(messageBoxText, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption, buttons, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption, buttons, icon, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(messageBoxText, caption, buttons, icon, defaultButton, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption, buttons, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption, buttons, icon, options);
+        }
+
+        /// <inheritdoc />
+        public WPFMessageBoxResult Show(Window owner, string messageBoxText, string caption, WPFMessageBoxButtons buttons, WPFMessageBoxImages icon, WPFMessageBoxResult defaultButton, WPFMessageBoxOptions options)
+        {
+            return WPFMessageBox.Show(owner, messageBoxText, caption, buttons, icon, defaultButton, options);
+        }
+    }
+}
